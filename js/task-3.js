338cfea7ccb65fcbf28d0767cc1d3beeff7eadb3
Mihail <mykh.shev4enko@gmail.com>
2024-10-27 20:53:25 +0200

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,7 @@
 function getElementWidth(content, padding, border) {
-  const widthContent = parseFloat(content);
-  const widthPadding = parseFloat(padding);
-  const widthBorder = parseFloat(border);
-
-  return widthContent + widthPadding * 2 + widthBorder * 2;
+  return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('60px', '12px', '8.5px'));
+console.log(getElementWidth('200px', '0px', '0px'));
